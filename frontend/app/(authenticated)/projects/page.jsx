f4b5de9f74b2d604c9ad62ddcdee1eb97df1fb49
AkifhanIlgaz <akifhanilgaz@gmail.com>
2024-02-21 15:50:13 +0300

    update

--- frontend/app/(authenticated)/projects/page.jsx
+++ frontend/app/(authenticated)/projects/page.jsx
@@ -4,17 +4,22 @@ import BreadCrumbs from '@/components/breadcrumbs'
 import { breadcrumbs, link } from '@/config/links'
 import { Button } from '@nextui-org/button'
 import { Link } from '@nextui-org/link'
+import { useTheme } from 'next-themes'
 
 export default function ProjectPage() {
-	const items = [breadcrumbs.projects, breadcrumbs.addProject]
+	const { theme } = useTheme()
+
+	const items = [breadcrumbs.projects]
 
 	return (
 		<div className="w-full h-full">
-			<BreadCrumbs items={items}></BreadCrumbs>
+			<div className="flex items-center justify-between">
+				<BreadCrumbs items={items}></BreadCrumbs>
 
-			<Button as={Link} href={link.addProject}>
-				Add project
-			</Button>
+				<Button as={Link} href={link.addProject} className="mr-6" color={theme == 'light' ? 'primary' : 'danger'}>
+					Add project
+				</Button>
+			</div>
 		</div>
 	)
 }
