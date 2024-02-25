de636592deff867bf9dea1cc277657815e70d06d
AkifhanIlgaz <akifhanilgaz@gmail.com>
2024-02-25 16:29:03 +0300

    get all projects

--- frontend/app/(authenticated)/projects/page.jsx
+++ frontend/app/(authenticated)/projects/page.jsx
@@ -1,14 +1,38 @@
 'use client'
 
 import BreadCrumbs from '@/components/breadcrumbs'
+import Loading from '@/components/loading'
+import api from '@/config/api'
 import { breadcrumbs, link } from '@/config/links'
+import firebaseClient from '@/lib/firebase'
 import { Button } from '@nextui-org/button'
 import { Link } from '@nextui-org/link'
+import axios from 'axios'
 import { useTheme } from 'next-themes'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import useSWR from 'swr'
 export default function ProjectPage() {
+	const [user, loading, error] = useAuthState(firebaseClient.auth)
+	const fetcher = async url => {
+		const idToken = await user.getIdToken(true)
+
+		const res = await axios.get(url, {
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${idToken}`
+			}
+		})
+
+		return res.data
+	}
+
+	const { data, isLoading } = useSWR(api.allProjects, fetcher)
+	console.log(data)
 	const { theme } = useTheme()
 
 	const items = [breadcrumbs.projects]
+
+	if (loading || isLoading) return <Loading />
 
 	return (
 		<div className="flex h-5 items-center justify-between">
--- frontend/config/api.js
+++ frontend/config/api.js
@@ -2,8 +2,9 @@ const baseUrl = 'http://localhost:8000/api'
 
 const api = {
 	projects: baseUrl + '/projects',
+	allProjects: baseUrl + '/projects/all',
 	addProject: baseUrl + '/projects/add',
-	addModerator: baseUrl + "/moderators/add"
+	addModerator: baseUrl + '/moderators/add'
 }
 
 export default api
