c4314386cb001b2a5ab842bff4b9f6c78a90fa2d
AkifhanIlgaz <akifhanilgaz@gmail.com>
2024-03-06 11:28:57 +0300

    update tasks table

--- frontend/components/project/tasks.jsx
+++ frontend/components/project/tasks.jsx
@@ -1,34 +1,9 @@
-import { Button, Chip, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow, getKeyValue } from '@nextui-org/react'
+import api from '@/config/api'
+import { Chip, Link, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from '@nextui-org/react'
 import { useCallback } from 'react'
 import { useForm } from 'react-hook-form'
+import useSWR from 'swr'
 import AddTask from '../modals/addTask'
-
-const rows = [
-	{
-		key: '1',
-		description: 'Join to discord',
-		role: 'CEO',
-		status: 'Active'
-	},
-	{
-		key: '2',
-		description: 'Zoey Lang',
-		role: 'Technical Lead',
-		status: 'Paused'
-	},
-	{
-		key: '3',
-		description: 'Jane Fisher',
-		role: 'Senior Developer',
-		status: 'Active'
-	},
-	{
-		key: '4',
-		description: 'William Howard',
-		role: 'Community Manager',
-		status: 'Vacation'
-	}
-]
 
 const columns = [
 	{
@@ -36,12 +11,12 @@ const columns = [
 		label: 'Description'
 	},
 	{
-		key: 'role',
-		label: 'ROLE'
+		key: 'createdAt',
+		label: 'Created At'
 	},
 	{
-		key: 'status',
-		label: 'STATUS'
+		key: 'url',
+		label: 'URL'
 	}
 ]
 
@@ -56,49 +31,35 @@ export default function Tasks({ projectId }) {
 		const cellValue = user[columnKey]
 
 		switch (columnKey) {
-			case 'description':
-				return cellValue
-			case 'role':
-				return (
-					<div className="flex flex-col">
-						<p className="text-bold text-small capitalize">{cellValue}</p>
-						<p className="text-bold text-tiny capitalize text-default-400">{user.team}</p>
-					</div>
-				)
 			case 'status':
 				return (
 					<Chip className="capitalize" color={statusColorMap[user.status]} size="sm" variant="flat">
 						{cellValue}
 					</Chip>
 				)
-			case 'actions':
+			case 'url':
 				return (
-					<div className="relative flex justify-end items-center gap-2">
-						<Dropdown>
-							<DropdownTrigger>
-								<Button isIconOnly size="sm" variant="light">
-									sdf
-								</Button>
-							</DropdownTrigger>
-							<DropdownMenu>
-								<DropdownItem>View</DropdownItem>
-								<DropdownItem>Edit</DropdownItem>
-								<DropdownItem>Delete</DropdownItem>
-							</DropdownMenu>
-						</Dropdown>
-					</div>
+					<Link href={cellValue} isExternal isBlock showAnchorIcon color="danger">
+						{cellValue}
+					</Link>
 				)
+			case 'createdAt':
+				const date = new Date(cellValue)
+				return date.toLocaleDateString()
 			default:
 				return cellValue
 		}
 	}, [])
 
+	const { data: tasks, isLoading } = useSWR(`${api.tasks}/${projectId}`)
+	if (isLoading) return
+	console.log(tasks)
 	return (
 		<>
 			<AddTask errors={errors} register={register} handleSubmit={handleSubmit} projectId={projectId} />
 			<Table aria-label="Example table with dynamic content">
 				<TableHeader columns={columns}>{column => <TableColumn key={column.key}>{column.label}</TableColumn>}</TableHeader>
-				<TableBody items={rows}>{item => <TableRow key={item.key}>{columnKey => <TableCell>{getKeyValue(item, columnKey)}</TableCell>}</TableRow>}</TableBody>
+				<TableBody items={tasks}>{item => <TableRow key={item.key}>{columnKey => <TableCell>{renderCell(item, columnKey)}</TableCell>}</TableRow>}</TableBody>
 			</Table>
 		</>
 	)
--- frontend/config/api.js
+++ frontend/config/api.js
@@ -5,7 +5,8 @@ const api = {
 	allProjects: baseUrl + '/projects/all',
 	addProject: baseUrl + '/projects/add',
 	addModerator: baseUrl + '/moderators/add',
-	addTask: baseUrl + '/tasks/add'
+	addTask: baseUrl + '/tasks/add',
+	tasks: baseUrl + '/tasks'
 }
 
 export default api
