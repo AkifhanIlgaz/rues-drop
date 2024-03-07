eaa1451767eca5257b0cc1e0848f0720c193e450
AkifhanIlgaz <akifhanilgaz@gmail.com>
2024-03-07 16:52:05 +0300

    update task

--- backend/models/task.model.go
+++ backend/models/task.model.go
@@ -6,11 +6,15 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+const StatusFinished = "Finished"
+const StatusInProgress = "In Progress"
+
 type Task struct {
 	Id          primitive.ObjectID `json:"id" bson:"_id,omitempty"`
 	ProjectId   primitive.ObjectID `json:"projectId" bson:"projectId"`
 	Description string             `json:"description"`
 	URL         string             `json:"url"`
+	Status      string             `json:"status"`
 	CreatedAt   time.Time          `json:"createdAt"`
 }
 
@@ -25,6 +29,7 @@ func (t TaskToAdd) ConvertToTask() Task {
 		ProjectId:   t.ProjectId,
 		Description: t.Description,
 		URL:         t.URL,
+		Status:      StatusInProgress,
 		CreatedAt:   time.Now(),
 	}
 }
