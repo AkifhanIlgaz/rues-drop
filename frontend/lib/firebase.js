1a6a546dc2fb7181a1147c94d9ec3e76bef20006
AkifhanIlgaz <akifhanilgaz@gmail.com>
2024-03-24 01:59:57 +0300

    fix .env bug with firebase

--- frontend/lib/firebase.js
+++ frontend/lib/firebase.js
@@ -3,11 +3,5 @@ import { getAuth } from 'firebase/auth'
 import firebase from 'firebase/compat/app'
 import 'firebase/compat/auth'
 
-const init = () => {
-	const config = JSON.parse(process.env.FIREBASE)
-
-	return firebase.initializeApp(config)
-}
-
-const app = init()
+const app = firebase.initializeApp(JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG))
 export const auth = getAuth(app)
